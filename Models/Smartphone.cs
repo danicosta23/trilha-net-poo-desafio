9d8b5bf19aeddbf581dea583e5a68e28b7218364
Daniela Souza <danicosta2302@gmail.com>
2024-01-23 21:44:30 -0300

    Desafio concluído!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,15 +1,23 @@
+using System.Runtime.CompilerServices;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        protected Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
+
+        public string Numero { get; set; }
+        protected string Modelo { get; set; }
+        protected string Imei { get; set; }
+        protected int Memoria { get; set;}
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama - OK
+
 
         public void Ligar()
         {
